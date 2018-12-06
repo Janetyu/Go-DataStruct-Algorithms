6cf73699f4fa2e395c9002fee9f6cc577891ff03
Janetyu <931242644@qq.com>
2018-12-06 15:33:06 +0800

    add array func with find and remove

--- Data-structures/array/array.go
+++ Data-structures/array/array.go
@@ -92,6 +92,87 @@ func (arr *Array) Set(index, e int) {
 	arr.data[index] = e
 }
 
+// 查找数组中是否有元素e
+func (arr *Array) Contains(e int) bool {
+	for _, v := range arr.data {
+		if e == v {
+			return true
+		}
+	}
+	return false
+}
+
+// 查找数组中元素e所在的索引，如果不存在元素e，则返回-1
+func (arr *Array) Find(e int) int {
+	for i, v := range arr.data {
+		if e == v {
+			return i
+		}
+	}
+	return -1
+}
+
+// 查找数组中所有元素e所在的索引，如果不存在元素e，则返回空slice
+func (arr *Array) FindAll(e int) []int {
+	indexs := []int{}
+	for i, v := range arr.data {
+		if e == v {
+			indexs = append(indexs, i)
+		}
+	}
+	return indexs
+}
+
+// 从数组中删除index位置的元素，返回删除的元素
+func (arr *Array) Remove(index int) int {
+	if index < 0 || index >= arr.size {
+		panic("Remove failed.Index is illegal.")
+	}
+	res := arr.data[index]
+	for i := index + 1; i < arr.size; i++ {
+		arr.data[i-1] = arr.data[i]
+	}
+	arr.size--
+	return res
+}
+
+// 从数组中删除第一个元素，返回删除的元素
+func (arr *Array) RemoveFirst() int {
+	return arr.Remove(0)
+}
+
+// 从数组中删除最后一个元素，返回删除的元素
+func (arr *Array) RemoveLast() int {
+	return arr.Remove(arr.size - 1)
+}
+
+// 从数组中删除元素e
+func (arr *Array) RemoveElement(e int) bool {
+	index := arr.Find(e)
+	if index != -1 {
+		arr.Remove(index)
+		return true
+	}
+	return false
+}
+
+// 从数组中删除所有的元素e
+func (arr *Array) RemoveAllElement(e int) bool {
+	indexs := arr.FindAll(e)
+
+	if len(indexs) == 0 {
+		//panic("Remove failed.Without the value in Array.")
+		return false
+	}
+
+	for _, v := range indexs {
+		arr.Remove(v)
+	}
+
+	return true
+
+}
+
 func (arr *Array) String() string {
 	var buffer bytes.Buffer
 	str1 := fmt.Sprintf("Array: size = %d, capacity = %d \n", arr.size, cap(arr.data))
--- Data-structures/array/array_test.go
+++ Data-structures/array/array_test.go
@@ -19,4 +19,18 @@ func Test_Array(t *testing.T) {
 
 	arr.Set(0, -1)
 	t.Log(arr.String())
+
+	ok := arr.RemoveAllElement(-2)
+	t.Log("Remove all: ", ok)
+	t.Log(arr.String())
+
+	ok = arr.RemoveAllElement(-1)
+	t.Log("Remove all: ", ok)
+	t.Log(arr.String())
+
+	arr.Remove(0)
+	t.Log(arr.String())
+
+	arr.RemoveElement(3)
+	t.Log(arr.String())
 }
