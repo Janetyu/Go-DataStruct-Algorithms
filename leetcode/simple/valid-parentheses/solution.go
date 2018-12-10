a0edaeb108e6d4ea0a89b56bf9be6d6a0e6228eb
Janetyu <931242644@qq.com>
2018-12-11 00:44:44 +0800

    valid_parentheses of leetcode

--- leetcode/simple/valid-parentheses/solution.go
+++ leetcode/simple/valid-parentheses/solution.go
@@ -1,5 +1,43 @@
 package valid_parentheses
 
-func isValid(s string) bool {
+/*
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
 
+有效字符串需满足：
+
+	1 左括号必须用相同类型的右括号闭合。
+	2 左括号必须以正确的顺序闭合。
+注意空字符串可被认为是有效字符串。
+ */
+
+// 有效的括号
+func isValid(s string) bool {
+	var topChar byte
+	m := map[byte]byte{')':'(','}':'{',']':'['}
+	b := []byte(s)
+	stack := []byte{}
+
+	for i := 0; i < len(s); i++ {
+		if b[i] == '(' || b[i] == '{' || b[i] == '[' {
+			stack = append(stack,b[i])
+		} else {
+			if len(stack) == 0 {
+				return false
+			}
+
+			topChar = stack[len(stack) - 1]
+			stack = stack[:len(stack) - 1]
+			if m[b[i]] == '(' && topChar != '(' {
+				return false
+			}
+			if m[b[i]] == '[' && topChar != '[' {
+				return false
+			}
+			if m[b[i]] == '{' && topChar != '{' {
+				return false
+			}
+		}
+	}
+
+	return len(stack) == 0
 }
